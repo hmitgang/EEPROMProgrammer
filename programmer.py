1bfeaba38e10f046bbf34c29497bea907a93424c
roxmysox <harrison@mitgangs.com>
2019-12-03 19:53:28 -0800

    Fixed constant variable scope

--- programmer.py
+++ programmer.py
@@ -3,28 +3,30 @@
 import RPi.GPIO as GPIO
 import sys, time
 
+
+CE = 2
+OE = 3
+WE = 4
+LOW  = 0
+HIGH = 1
+
+# [A0, A1,..., A12, A13, A14]
+addressPins = [10, 9, 11, 25, 8, 7, 5, 6, 12, 13, 19, 26, 16, 20, 21]
+
+# [D0, D1, D2, D3, D4, D5, D6, D7]
+dataPins = [14, 15, 18, 17, 27, 22, 23, 24]
+
+
 def setup():
 	GPIO.setmode(GPIO.BCM) #Use chip numbering scheme
 	GPIO.setwarnings(False)
 
 
-	CE = 2
-	OE = 3
-	WE = 4
-	LOW  = 0
-	HIGH = 1
 
 	# Setup CE, OE, and WE
 	GPIO.setup(CE, GPIO.OUT, initial=HIGH)
 	GPIO.setup(OE, GPIO.OUT, initial=HIGH)
 	GPIO.setup(WE, GPIO.OUT, initial=HIGH)
-
-
-	# [A0, A1,..., A12, A13, A14]
-	addressPins = [10, 9, 11, 25, 8, 7, 5, 6, 12, 13, 19, 26, 16, 20, 21]
-
-	# [D0, D1, D2, D3, D4, D5, D6, D7]
-	dataPins = [14, 15, 18, 17, 27, 22, 23, 24]
 
 
 	#Set the chip in standby mode while setting up the rest GPIO:
